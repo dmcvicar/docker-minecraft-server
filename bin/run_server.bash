751f55117bb635ecb90e65e7bc63ff4e8994357a
dmcvicar <dwmcvicar@gmail.com>
2020-06-01 14:22:19 -0600

    better maintained world versions etc

--- bin/run_server.bash
+++ bin/run_server.bash
@@ -1,33 +1,32 @@
 #!/bin/bash
 echo 'Running Server' &&
-if [[ ! -f ./world-version ]]; then
-  echo 0 > world-version
+if [[ ! -f ./world-versions/${WORLD_NAME}/world-version ]]; then
+  mkdir -p ./world-versions/${WORLD_NAME} &&
+  echo 0 > ./world-versions/${WORLD_NAME}/world-version
 fi  &&
 
-aws s3 cp s3://minecraft-mcvicaria/server-$MINECRAFT_VERSION/world-version ./remote-world-version &&
-REMOTE_VERSION=`cat ./remote-world-version | sed 's/\r$//'` &&
-MY_VERSION=`cat ./world-version` &&
+aws s3 cp s3://minecraft-mcvicaria/server-${MINECRAFT_VERSION}/world-versions/${WORLD_NAME}/world-version ./world-versions/${WORLD_NAME}/remote-world-version &&
+REMOTE_VERSION=`cat ./world-versions/${WORLD_NAME}/remote-world-version | sed 's/\r$//'` &&
+MY_VERSION=`cat ./world-versions/${WORLD_NAME}/world-version` &&
 echo "Remote version: $REMOTE_VERSION" &&
 echo "My version: $MY_VERSION" &&
 if [ "$REMOTE_VERSION" -gt "$MY_VERSION" ]; then
+        MY_VERSION=$REMOTE_VERSION &&
         echo "Downloading latest world." &&
         rm -rf ./$WORLD_NAME &&
-        aws s3 sync s3://minecraft-mcvicaria/server-$MINECRAFT_VERSION/$WORLD_NAME ./$WORLD_NAME &&
-        rm -f ./world-version &&
-        mv ./remote-world-version ./world-version &&
-        MY_VERSION=$REMOTE_VERSION
+        aws s3 sync s3://minecraft-mcvicaria/server-${MINECRAFT_VERSION}/${WORLD_NAME} ./${WORLD_NAME}
 fi &&
-if [[ -f ./remote-world-version ]]; then
-        rm ./remote-world-version
+if [[ -f ./world-versions/${WORLD_NAME}/remote-world-version ]]; then
+        rm ./world-versions/${WORLD_NAME}/remote-world-version
 fi &&
 MY_VERSION=$(($MY_VERSION+1)) &&
-echo $MY_VERSION > ./world-version &&
+echo $MY_VERSION > ./world-versions/${WORLD_NAME}/world-version &&
 
-sed -e "s/{{level_name}}/${WORLD_NAME}/g" -e "s/{{rcon_pwd}}/${MCRCON_PWD}/g" server.properties.tmpl > server.properties
+sed -e "s/{{level_name}}/${WORLD_NAME}/g" -e "s/{{rcon_pwd}}/${MCRCON_PWD}/g" server.properties.tmpl > server.properties &&
 
-java -Xmx1024M -Xms1024M -jar server.jar nogui;
+java -Xmx4096M -Xms4096M -jar server.jar nogui &&
 
-aws s3 rm s3://minecraft-mcvicaria/server-$MINECRAFT_VERSION/$WORLD_NAME --recursive;
-aws s3 cp ./$WORLD_NAME s3://minecraft-mcvicaria/server-$MINECRAFT_VERSION/$WORLD_NAME --recursive;
-aws s3 cp ./world-version s3://minecraft-mcvicaria/server-$MINECRAFT_VERSION/world-version;
+aws s3 rm s3://minecraft-mcvicaria/server-${MINECRAFT_VERSION}/${WORLD_NAME} --recursive &&
+aws s3 cp ./${WORLD_NAME} s3://minecraft-mcvicaria/server-${MINECRAFT_VERSION}/${WORLD_NAME} --recursive &&
+aws s3 cp ./world-versions/${WORLD_NAME}/world-version s3://minecraft-mcvicaria/server-${MINECRAFT_VERSION}/world-versions/${WORLD_NAME}/world-version;
 echo 'Goodbye';
