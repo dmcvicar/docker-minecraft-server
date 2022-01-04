fa1481248c65cb90fea1e76755047cd7ae8a630f
David McVicar <dwmcvicar@gmail.com>
2022-01-03 19:11:34 -0700

    update 01/03/2022

--- bin/run_server.bash
+++ bin/run_server.bash
@@ -3,21 +3,24 @@ echo 'Running Server' &&
 if [[ ! -f ./world-versions/${WORLD_NAME}/world-version ]]; then
   mkdir -p ./world-versions/${WORLD_NAME} &&
   echo 0 > ./world-versions/${WORLD_NAME}/world-version
-fi  &&
-
-aws s3 cp s3://minecraft-mcvicaria/server-${MINECRAFT_VERSION}/world-versions/${WORLD_NAME}/world-version ./world-versions/${WORLD_NAME}/remote-world-version &&
-REMOTE_VERSION=`cat ./world-versions/${WORLD_NAME}/remote-world-version | sed 's/\r$//'` &&
-MY_VERSION=`cat ./world-versions/${WORLD_NAME}/world-version` &&
-echo "Remote version: $REMOTE_VERSION" &&
-echo "My version: $MY_VERSION" &&
-if [ "$REMOTE_VERSION" -gt "$MY_VERSION" ]; then
-        MY_VERSION=$REMOTE_VERSION &&
-        echo "Downloading latest world." &&
-        rm -rf ./$WORLD_NAME &&
-        aws s3 sync s3://minecraft-mcvicaria/server-${MINECRAFT_VERSION}/${WORLD_NAME} ./${WORLD_NAME}
+  CREATE_WORLD="true"
 fi &&
+
+if [ -z $CREATE_WORLD ]
+  aws s3 cp s3://mcvicar-minecraft/server-${MINECRAFT_VERSION}/world-versions/${WORLD_NAME}/world-version ./world-versions/${WORLD_NAME}/remote-world-version
+  REMOTE_VERSION=`cat ./world-versions/${WORLD_NAME}/remote-world-version | sed 's/\r$//'` &&
+else
+  REMOTE_VERSION=-1
+fi && 
+MY_VERSION=`cat ./world-versions/${WORLD_NAME}/world-version` &&
+if [ "$REMOTE_VERSION" -gt "$MY_VERSION" ]; then
+  MY_VERSION=$REMOTE_VERSION &&
+  echo "Downloading latest world." &&
+  rm -rf ./$WORLD_NAME &&
+  aws s3 sync s3://mcvicar-minecraft/server-${MINECRAFT_VERSION}/${WORLD_NAME} ./${WORLD_NAME}
+fi && 
 if [[ -f ./world-versions/${WORLD_NAME}/remote-world-version ]]; then
-        rm ./world-versions/${WORLD_NAME}/remote-world-version
+  rm ./world-versions/${WORLD_NAME}/remote-world-version
 fi &&
 MY_VERSION=$(($MY_VERSION+1)) &&
 echo $MY_VERSION > ./world-versions/${WORLD_NAME}/world-version &&
@@ -26,7 +29,6 @@ sed -e "s/{{level_name}}/${WORLD_NAME}/g" -e "s/{{rcon_pwd}}/${MCRCON_PWD}/g" se
 
 java -Xmx4096M -Xms4096M -jar server.jar nogui &&
 
-aws s3 rm s3://minecraft-mcvicaria/server-${MINECRAFT_VERSION}/${WORLD_NAME} --recursive &&
-aws s3 cp ./${WORLD_NAME} s3://minecraft-mcvicaria/server-${MINECRAFT_VERSION}/${WORLD_NAME} --recursive &&
-aws s3 cp ./world-versions/${WORLD_NAME}/world-version s3://minecraft-mcvicaria/server-${MINECRAFT_VERSION}/world-versions/${WORLD_NAME}/world-version;
+aws s3 sync ./${WORLD_NAME} s3://mcvicar-minecraft/server-${MINECRAFT_VERSION}/${WORLD_NAME} --delete &&
+aws s3 cp ./world-versions/${WORLD_NAME}/world-version s3://mcvicar-minecraft/server-${MINECRAFT_VERSION}/world-versions/${WORLD_NAME}/world-version;
 echo 'Goodbye';
